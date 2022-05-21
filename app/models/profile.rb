9d50ea8b2d179199b533847fff9b1be7f772e90a
Dinorah Skidelsky <dinorahskidelsky@gmail.com>
2022-05-21 18:05:02 -0300

    front views devise & back

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,7 @@ class Profile < ApplicationRecord
   has_many :reviews, through: :reservations
   has_many :chatrooms
   validates :description, :location, :price, :phone_number, :photos, presence: true
+  validates :user_id, uniqueness: true
   has_many_attached :photos
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
